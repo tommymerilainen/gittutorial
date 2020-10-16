f4f95936673575301a9f77f5cbb847e2da892ae4
tommymerilainen <tmerilai20@student.oulu.fi>
2020-10-16 14:25:21 +0300

    123123

--- mathtools.py
+++ mathtools.py
@@ -16,11 +16,3 @@ def factorial(n):
     else:
         return n * factorial(n-1)
         
-def fib(n):
-    ''' Calculates the n value of the fibonacci sequence'''
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fib(n-1)+fib(n-2)
